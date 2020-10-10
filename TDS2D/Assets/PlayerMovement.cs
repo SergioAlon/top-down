ddca0aaa6b3cd6418294f62f3ead1d386f7b3c14
Sergio <sergio.alonso@digipen.edu>
2020-10-10 17:04:02 +0200

    Aimming and shooting added

--- TDS2D/Assets/PlayerMovement.cs
+++ TDS2D/Assets/PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
@@ -16,11 +18,15 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        // Input.mousePosition
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
