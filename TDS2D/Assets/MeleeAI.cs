65f944b422d3dc1263b11dd7c39dc6019042ec90
Aitor Pinilla <aitor.pinilla@digipen.edu>
2020-10-13 20:48:50 +0200

    Enemy AI v1

---
+++ TDS2D/Assets/MeleeAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAI : MonoBehaviour
+{
+    public float speed;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+
+
+
+    public Transform player;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
